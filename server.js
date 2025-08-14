873a8af8c2951d223816c49dffd67ff9aace0092
hassanrazak <76017055+hassanrazak@users.noreply.github.com>
2025-08-14 10:57:36 -0400

    lab1 completed

---
+++ server.js
@@ -0,0 +1,21 @@
+// require
+
+const express = require("express")
+const path = require("path")
+
+const app = express()
+
+const port = 3000
+
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname,"public","index.html"))
+})
+
+app.get("/contact", (req, res) => {
+    res.sendFile(path.join(__dirname,"public","contact.html"))
+})
+
+app.listen(port,() => {
+    console.log(`server running at http://localhost:${port}`)
+})
